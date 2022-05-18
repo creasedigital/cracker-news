d1f755fc448d824971f37838771e657eabc2b6af
creasedigital <ojorechris@gmail.com>
2022-05-18 10:59:23 +0100

    API fetching service for story completed

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,11 +1,12 @@
-import { getStoryIds } from "../services/hnApi";
+import { getStoryIds, getStory } from "../services/hnApi";
 import { useState, useEffect } from "react";
 import { Box } from "@chakra-ui/react";
 export const StoriesContainer = () => {
 	const [storyIds, setStoryIds] = useState([]);
 
 	useEffect(() => {
-		getStoryIds().then((res) => setStoryIds(res));
+		getStoryIds().then((data) => setStoryIds(data));
+		storyIds && getStory(storyIds[0]).then((data) => console.log(data));
 	}, []);
 
 	return <Box>{JSON.stringify(storyIds)}</Box>;
