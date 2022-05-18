0fcf80de076fd1bcd68b98e3fa7fbe0854b2edea
creasedigital <ojorechris@gmail.com>
2022-05-18 01:40:24 +0100

    setting up for API consumption

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { getStoryIds } from "./services/hnApi";
 
 function App() {
-	return <Box>Hello</Box>;
+	const [storyIds, setStoryIds] = useState([]);
+
+	useEffect(() => {
+		getStoryIds().then((res) => setStoryIds(res));
+	}, []);
+
+	return <Box>{storyIds}</Box>;
 }
 
 export default App;
