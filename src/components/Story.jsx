cf9e7679ab87151379eceab959d675b021b4bfdf
creasedigital <ojorechris@gmail.com>
2022-05-18 15:27:09 +0100

    star ratings added

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,3 +1,5 @@
+import { Box, Link, Flex } from "@chakra-ui/react";
+import ReactStars from "react-stars";
 import { useState, useEffect } from "react";
 import { getStoryIds, getStory } from "../services/hnApi";
 
@@ -5,8 +7,25 @@ export const Story = ({ storyId }) => {
 	const [story, setStory] = useState({});
 
 	useEffect(() => {
-		getStory(storyId);
+		getStory(storyId).then((data) => data && data.url && setStory(data));
 	}, []);
 
-	return <div> This is an ID: {storyId}</div>;
+	// return JSON.stringify(story);
+
+	return story && story.url ? (
+		<>
+			<Box>
+				<Link href={story.url}>
+					<Box as="p">{story.title}</Box>
+				</Link>
+			</Box>
+			<Flex align="center" justify="space-between" m={10}>
+				<Flex align="center">
+					Stars:{" "}
+					{<ReactStars count={story.score} size={32} color1={"#ff3366"} />}
+				</Flex>
+				<Box>Author: {story.by}</Box>
+			</Flex>
+		</>
+	) : null;
 };
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -11,5 +11,5 @@ export const StoriesContainer = () => {
 		// storyIds && getStory(storyIds[0]).then((data) => console.log(data));
 	}, []);
 
-	return storyIds.map((storyId) => <Story storyId={storyId} />);
+	return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
 };
