11000197993efb0ab26c66016d61134661a9cbae
creasedigital <ojorechris@gmail.com>
2022-05-19 16:04:45 +0100

    story styling adjusted

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -13,19 +13,23 @@ export const Story = ({ storyId }) => {
 	// return JSON.stringify(story);
 
 	return story && story.url ? (
-		<>
+		<Flex flexDirection="column" align="start" bg="#AFBE87" px={10} my={8}>
 			<Box>
-				<Link href={story.url}>
+				<Link href={story.url} textDecoration="none">
 					<Box as="p">{story.title}</Box>
 				</Link>
 			</Box>
-			<Flex align="center" justify="space-between" m={10}>
-				<Flex justify="center" direction="column">
-					Stars:{" "}
-					{<RatingStar maxScore={5} rating={story.score} id={story.id} />}
+			<Flex w="75%" align="center" justify="space-between" my={4}>
+				<Flex align="center" justify="space-between">
+					<Box as="p" fontStyle="italic">
+						Stars:{" "}
+					</Box>
+					<Box>
+						{<RatingStar maxScore={5} rating={story.score} id={story.id} />}
+					</Box>
 				</Flex>
 				<Box>Author: {story.by}</Box>
 			</Flex>
-		</>
+		</Flex>
 	) : null;
 };
