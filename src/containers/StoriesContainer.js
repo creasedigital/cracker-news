3f810e93f631d1a176a8db9beacc801fab58a19b
creasedigital <ojorechris@gmail.com>
2022-05-20 10:49:55 +0100

    define heading

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,7 +1,7 @@
 import { getStoryIds, getStory } from "../services/hnApi";
 import { Story } from "../components/Story";
 import { useState, useEffect } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 export const StoriesContainer = () => {
 	const [storyIds, setStoryIds] = useState([]);
@@ -11,5 +11,14 @@ export const StoriesContainer = () => {
 		// storyIds && getStory(storyIds[0]).then((data) => console.log(data));
 	}, []);
 
-	return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
+	return (
+		<>
+			<Heading as="h1" mb={2} color="#2e2e2c">
+				Cracker New Stories
+			</Heading>
+			{storyIds.map((storyId) => (
+				<Story key={storyId} storyId={storyId} />
+			))}
+		</>
+	);
 };
